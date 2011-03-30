26ad01bdfc47eca84aa6e2749045ee43da6e4b59
fl4t <julien.stechele@gmail.com>
2011-03-30 16:32:13 +0200

    fonction de recuperation du prix qui bug

--- traitement-commande.php
+++ traitement-commande.php
@@ -8,40 +8,41 @@
             <p>
                Veuillez-vérifiez les informations précédemment choisi :
             </p>
+            <p>
             <?php
                // affichage pour savoir si c'est fait.
-               echo '<pre>';
-               echo 'POST : <br />';
-               print_r($_POST);
-               echo '</pre>';
+               //echo '<pre>';
+               //echo 'POST : <br />';
+               //print_r($_POST);
+               //echo '</pre>';
                
                // On apelle la fonction de transfert session <- post.
-               F_TransfertPostDansSession();
-               // On utilise la fonction array_count() pour savoir combien de produits sont valides.
-               $intNombreDeProduitsValide = array_count($_SESSION['panier']['DesignProduit']); 
-               echo $intNombreDeProduitsValide;
-               //for ($intAffichageProduit;$intNombreDeProduitsValide;$intAffichageProduit++)
+               $pute = PrixDuProduit(design1);
+               echo $pute;
+               //TransfertPostDansSession();
+               //echo '<table>';
+               //echo '<tr>';
+               //echo '<td>Cette commande est préparé pour le restaurant <strong>' . $_SESSION['Restaurant'] . '</strong></td>';
+               //echo '</tr>';
+               //echo '<tr>';
+               //echo '<td>Produits :</td>';
+               //echo '<td>Quantité :</td>';
+               //echo '<td>Prix Unitaire :</td>';
+               //echo '<td>Prix hors taxes :</td>';
+               //echo '</tr>';
+               //// On affiche le panier.
+               //$intNombreDeProduits = count($_SESSION['Panier']['DesignProduit']);
+               //for($intAffichage;$intAffichage <= $intNombreDeProduits;$intAffichage++)
                //{
-                  //if (isset($_SESSION['']
-                  //// Si la quantité est au dessus de 1 et que c'est raisonable, on affiche dans un tableau HTML.
-                  //if (isset($_SESSION['textQuantiteProduit1']) >= 1 AND $_SESSION['textQuantiteProduit1'] <= 100)
-                  //{
-                     //// Si on est dans la fourchette de valeurs.
-                     //$fltPrixHT = PrixListeProduits1('ListeProduits1') * $_SESSION['QantiteListeProduits1'];
-                     //echo '<table>';
-                     //echo '<tr>';
-                     //echo '<td>' . $_SESSION['ListeProduits1'] . '</td>';
-                     //echo '<td>' . $_SESSION['QantiteListeProduits1'] . '</td>';
-                     //echo '<td>' . $fltPrixHT . '<td>';
-                     //echo '</tr>';
-                     //echo '</table>';
-                  //}
-                  //else
-                  //{
-                     //// Soit c'est 0 soit on est au dessus de 100.
-                     //echo 'pas raisonable !';
-                  //}
+                  //echo '<tr>';
+                  //echo '<td>' . $_SESSION['Panier']['DesignProduit'][$intAffichage] . '</td>';
+                  //echo '<td>' . $_SESSION['Panier']['QuantiteProduit'][$intAffichage] . '</td>';
+                  //echo '<td>' . $_SESSION['Panier']['PrixProduit'][$intAffichage] . '</td>';
+                  //echo '<td>' . $_SESSION['Panier']['PrixProduit'][$intAffichage] * $_SESSION['Panier']['QuantiteProduit'][$intAffichage] . '</td>';
+                  //echo '</tr>';
                //}
+               //echo '</table>';
             ?>
+            <p>
          </div>
          <?php include("footer.php");?>
