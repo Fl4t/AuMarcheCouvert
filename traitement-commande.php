d432a66a827ad0dd1780d76a8a6c873c198bc851
fl4t <julien.stechele@gmail.com>
2011-03-30 22:35:00 +0200

    Bonne affichage et gere le comportement des champs vides

--- traitement-commande.php
+++ traitement-commande.php
@@ -5,44 +5,54 @@
          </nav>
          <div id="texte">
             <!-- Contenu de la page traitement-commande.php --> 
-            <p>
-               Veuillez-vérifiez les informations précédemment choisi :
-            </p>
-            <p>
             <?php
                // affichage pour savoir si c'est fait.
                //echo '<pre>';
                //echo 'POST : <br />';
                //print_r($_POST);
                //echo '</pre>';
-               
-               // On apelle la fonction de transfert session <- post.
-               $pute = PrixDuProduit(design1);
-               echo $pute;
-               //TransfertPostDansSession();
-               //echo '<table>';
-               //echo '<tr>';
-               //echo '<td>Cette commande est préparé pour le restaurant <strong>' . $_SESSION['Restaurant'] . '</strong></td>';
-               //echo '</tr>';
-               //echo '<tr>';
-               //echo '<td>Produits :</td>';
-               //echo '<td>Quantité :</td>';
-               //echo '<td>Prix Unitaire :</td>';
-               //echo '<td>Prix hors taxes :</td>';
-               //echo '</tr>';
-               //// On affiche le panier.
-               //$intNombreDeProduits = count($_SESSION['Panier']['DesignProduit']);
-               //for($intAffichage;$intAffichage <= $intNombreDeProduits;$intAffichage++)
-               //{
-                  //echo '<tr>';
-                  //echo '<td>' . $_SESSION['Panier']['DesignProduit'][$intAffichage] . '</td>';
-                  //echo '<td>' . $_SESSION['Panier']['QuantiteProduit'][$intAffichage] . '</td>';
-                  //echo '<td>' . $_SESSION['Panier']['PrixProduit'][$intAffichage] . '</td>';
-                  //echo '<td>' . $_SESSION['Panier']['PrixProduit'][$intAffichage] * $_SESSION['Panier']['QuantiteProduit'][$intAffichage] . '</td>';
-                  //echo '</tr>';
-               //}
-               //echo '</table>';
+               TransfertPostDansSession(); // On apelle la fonction de transfert session <- post.
+               $intNombreDeProduits = count($_SESSION['Panier']['DesignProduit']);
+               if ($intNombreDeProduits != 0) 
+               {
+                  echo '<table>';
+                  echo '<h3><strong>' . $_SESSION['NomRestaurant'] . '</strong></h3>';
+                  echo '<thead>';
+                  echo '<tr>';
+                  echo '<td class="recapitulatif">Produits :</td>';
+                  echo '<td class="recapitulatif">Quantité :</td>';
+                  echo '<td class="recapitulatif">Prix Unitaire :</td>';
+                  echo '<td class="recapitulatif">Prix HT :</td>';
+                  echo '</tr>';
+                  echo '</thead>';
+                  echo '<tbody>';
+                  // On affiche le panier.
+                  for($intAffichage=1;$intAffichage <= $intNombreDeProduits -1;$intAffichage++)
+                  {
+                     echo '<tr>';
+                     echo '<td class="recapitulatif">' . $_SESSION['Panier']['DesignProduit'][$intAffichage] . '</td>';
+                     echo '<td class="recapitulatif">' . $_SESSION['Panier']['QuantiteProduit'][$intAffichage] . '</td>';
+                     echo '<td class="recapitulatif">' . $_SESSION['Panier']['PrixProduit'][$intAffichage] . '</td>';
+                     echo '<td class="recapitulatif">' . $_SESSION['Panier']['PrixProduit'][$intAffichage] * $_SESSION['Panier']['QuantiteProduit'][$intAffichage] . '</td>';
+                     echo '</tr>';
+                  }
+                  echo '<tr>'; // on affiche le dernier séparement pour ne pas avoir de border-bottom.
+                  echo '<td class="centrer">' . $_SESSION['Panier']['DesignProduit'][$intNombreDeProduits] . '</td>';
+                  echo '<td class="centrer">' . $_SESSION['Panier']['QuantiteProduit'][$intNombreDeProduits] . '</td>';
+                  echo '<td class="centrer">' . $_SESSION['Panier']['PrixProduit'][$intNombreDeProduits] . '</td>';
+                  echo '<td class="centrer">' . $_SESSION['Panier']['PrixProduit'][$intNombreDeProduits] * $_SESSION['Panier']['QuantiteProduit'][$intNombreDeProduits] . '</td>';
+                  echo '</tr>';
+                  echo '</tbody>';
+                  echo '</table>';
+               } 
+               else 
+               {
+                  echo '<p class="centrer">';
+                  echo 'Les champs n\'ont pas été correctement saisis.';
+                  echo '<br />';
+                  echo '<a href=commande.php>Retour</a>';
+                  echo '</p>';
+               }
             ?>
-            <p>
          </div>
          <?php include("footer.php");?>
