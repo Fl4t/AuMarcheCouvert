9bb080f00f42b3395c728353135cf9b1a8d5d569
fl4t <julien.stechele@gmail.com>
2011-04-10 00:09:05 +0200

    changement des noms, nouvelles fonctions, résolution de bug

--- fonctions.php
+++ fonctions.php
@@ -17,7 +17,7 @@ define('intTVA',1.055);
 // Fonction ConnexionBDD
 // On se connecte sur la base de donnée en passant par un essai
 // affiche une erreur explicite.
-function ConnexionBDD()
+function F_ConnexionBDD()
 {
    try
    {
@@ -32,7 +32,7 @@ function ConnexionBDD()
 
 // Fonction TransfertPostSession
 // On affecte toutes les valeurs de la superglobale $_POST à $_SESSION si ils sont valide. 
-function TransfertPostDansSession()
+function P_TransfertPostDansSession()
 {
    // Si le restaurant a été renseigné, on continue.
    if ($_POST['listeRestaurant'] != "Vide")
@@ -45,7 +45,10 @@ function TransfertPostDansSession()
          // Si le produit a été séléctionné et qu'une quantité a été tapé et qu'elle est supérieur à 1.
          if ($_POST['listeProduits' . $NombreDeProduits] != "Vide")
          {
-            if ($_POST['QuantiteProduit' . $NombreDeProduits] != 0)
+            if (empty($_POST['QuantiteProduit'.$NombreDeProduits]) || empty($_POST['PrixDuJour'.$NombreDeProduits]))
+            {
+            }
+            else
             {
                // On incrémente le compteur quand les conditions sont vraies.
                $intCompteurAjout += 1;
@@ -53,27 +56,48 @@ function TransfertPostDansSession()
                $_SESSION['Panier']['DesignProduit'][$intCompteurAjout] = $_POST['listeProduits' . $NombreDeProduits];
                // On remplie le tableau 'QuantiteProduit' qui ce trouve lui même dans le tabeau à l'index 1 de 'Panier'
                // On force tout ce qui a pu être tapé en entier, si c'était autre chose qu'un entier, ça vaudra 0 
-               $_SESSION['Panier']['QuantiteProduit'][$intCompteurAjout] = (integer) ($_POST['QuantiteProduit' . $NombreDeProduits]);
+               $_SESSION['Panier']['QuantiteProduit'][$intCompteurAjout] = (integer) $_POST['QuantiteProduit' . $NombreDeProduits];
                // On ajoute le prix du produit via la fonction PrixDuProduit.
-               $_SESSION['Panier']['PrixProduit'][$intCompteurAjout] = PrixDuProduit($_SESSION['Panier']['DesignProduit'][$intCompteurAjout]);
+               // J'utilise une expréssion régulière au cas ou une virgule est utilisé, les décimaux php n'accepte que le point.
+               $_SESSION['Panier']['PrixDuJour'][$intCompteurAjout] = preg_replace('#,#','.',$_POST['PrixDuJour'.$NombreDeProduits]);
             }
          }
       }
    }
+   $_SESSION['NombreDeProduits'] = count($_SESSION['Panier']['DesignProduit']);
    // affichage pour savoir si c'est fait.
-   //echo '<pre>';
-   //echo 'SESSION : <br />';
-   //print_r($_SESSION);
-   //echo '</pre>';
+   echo '<pre>';
+   echo 'SESSION : <br />';
+   print_r($_SESSION);
+   echo '</pre>';
 }
-
-function PrixDuProduit($NomProduit)
+function F_CodeDuRestaurant($strNomRestaurant)
 {
    global $objBDD; // On travaille sur la variable globale $objBDD.
-   // On va chercher le prix du produits contenu en paramètre
-   $objPrixProduits = $objBDD->query('SELECT PrixProduit FROM Produits WHERE DesignProduit = \''.$NomProduit.'\'') or die(print_r($objBDD->errorInfo()));
-   $PrixProduit = $objPrixProduits->fetch();
-   $objPrixProduits->closeCursor();
-   return $PrixProduit['PrixProduit']; // On retourne la valeur du champ PrixProduit
+   // On recupère le code du restaurant qui passe commande.
+   $objCodeRestaurant = $objBDD->query('SELECT CodeRestaurant FROM Restaurants WHERE NomRestaurant = \''.$strNomRestaurant.'\'') or die(print_r($objBDD->errorInfo()));
+   $strCodeRestaurant = $objCodeRestaurant->fetch();
+   $objCodeRestaurant->closeCursor(); // Termine le traitement de la requête
+   return $strCodeRestaurant['CodeRestaurant']; // On retourne la valeur du champ CodeRestaurant
 }
+
+function F_RefDuProduit($strNomProduit)
+{
+   global $objBDD; // On travaille sur la variable globale $objBDD.
+   // On va chercher la référence du produits
+   $objRefProduit = $objBDD->query('SELECT RefProduit FROM Produits WHERE DesignProduit = \''.$strNomProduit.'\'') or die(print_r($objBDD->errorInfo()));
+   $intRefProduit = $objRefProduit->fetch();
+   $objRefProduit->closeCursor();
+   return $intRefProduit['RefProduit'];
+}
+
+//function LectureBDD($strRequete)
+//{
+   //global $objBDD; // On travaille sur la variable globale $objBDD.
+   //// On va chercher le prix du produits contenu en paramètre
+   //$objValeurRecolter = $objBDD->query($strRequete) or die(print_r($objBDD->errorInfo()));
+   //$tableauValeurRecolter = $objPrixProduits->fetch();
+   //$objValeurRecolter->closeCursor();
+   //return $tableauValeurRecolter; // On retourne la valeur du champ PrixProduit
+/*}*/
 ?>
