2cb8c173e66ec165afb18eccae82fbab850516d2
fl4t <julien.stechele@gmail.com>
2011-04-10 00:07:54 +0200

    Prochain travail...

---
+++ consultation.php
@@ -0,0 +1,9 @@
+<?php include("header.php");?>
+               <li><a href="index.php">Accueil</a></li>
+               <li><a href="commande.php">Nouvelle Commande</a></li>
+               <li class="actif"><a href="consultation.php">Consultation</a></li>
+            </ul>
+         </nav>
+         <div id="texte">
+         </div>
+         <?php include("footer.php");?>
