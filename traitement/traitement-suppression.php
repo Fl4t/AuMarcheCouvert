b59bb028825bd95d92e6ab097ed82451816c1c6a
fl4t <julien.stechele@gmail.com>
2011-04-13 16:04:47 +0200

    Correction des liens URL absolu

--- traitement/traitement-suppression.php
+++ traitement/traitement-suppression.php
@@ -1,15 +1,15 @@
 <?php include("../header.php");?>
-               <li><a href="index.php">Accueil</a></li>
-               <li><a href="commande.php">Nouvelle Commande</a></li>
-               <li><a href="consultation.php">Consultation</a></li>
-               <li class="actif"><a href="administration.php">Administration</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/index.php">Accueil</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/commande.php">Nouvelle Commande</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/consultation.php">Consultation</a></li>
+               <li class="actif"><a href="http://localhost:8888/AuMarcheCouvert/administration.php">Administration</a></li>
             </ul>
          </nav>
          <div id="panneau">
             <ul>
-               <li><a href="ajout.php">Suppression</a></li>
-               <li><a href="modification.php">Modification</a></li>
-               <li class="actif"><a href="suppression.php">Suppression</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/ajout.php">Ajout</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/modification.php">Modification</a></li>
+               <li class="actif"><a href="http://localhost:8888/AuMarcheCouvert/suppression.php">Suppression</a></li>
             </ul>
          </div>
          <div id="texte-administration">
