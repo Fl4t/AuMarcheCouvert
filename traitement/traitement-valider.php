ce3617a8304e33677060b5609375026a590cb8b5
fl4t <julien.stechele@gmail.com>
2011-04-16 21:10:38 +0200

    lien URL absolu, simplification

--- traitement/traitement-valider.php
+++ traitement/traitement-valider.php
@@ -1,42 +1,35 @@
 <?php include("../header.php");?>
-               <li><a href="index.php">Accueil</a></li>
-               <li class="actif"><a href="commande.php">Nouvelle Commande</a></li>
-               <li><a href="consultation.php">Consultation</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/index.php">Accueil</a></li>
+               <li class="actif"><a href="http://localhost:8888/AuMarcheCouvert/commande.php">Nouvelle Commande</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/consultation.php">Consultation</a></li>
+               <li><a href="http://localhost:8888/AuMarcheCouvert/administration.php">Administration</a></li>
             </ul>
          </nav>
          <div id="texte">
             <?php
                if ($_POST['valider'] == "Oui")
                {
-                  try
+                  // Appel de la fonction F_CodeDuRestaurant
+                  // stripslashes au cas ou un restaurant est un appostrophe.
+                  $intCodeRestaurant = F_CodeDuRestaurant(stripslashes($_SESSION['NomRestaurant']));
+                  P_CreationCommande($intCodeRestaurant); // Appel de la fonction de création de la commande.
+                  $intDerniereID = (integer) $objBDD->lastInsertId(); // On récupère la valeur de la dernière insertion id (string) car comme c'est en auto-increment, on ne connaissai pas la valeur.
+                  for($intNombreDeProduits=1;$intNombreDeProduits <= $_SESSION['NombreDeProduits'];$intNombreDeProduits++)
                   {
-                     // Appel de la fonction F_CodeDuRestaurant
-                     $intCodeRestaurant = F_CodeDuRestaurant($_SESSION['NomRestaurant']);
-                     // On ajoute une entrée dans la table commande. Il n'y a rien a la première , car c'est auto-incrementé.
-                     $objBDD->exec('INSERT INTO Commandes(NumCommande, CodeRestaurant, DateCommande) VALUES(\'\','.$intCodeRestaurant.',NOW())');
-                     $intDerniereID = (integer) $objBDD->lastInsertId(); // On récupère la valeur de la dernière insertion id (string) car comme cetait en auto-increment, on ne connaissai pas la valeur.
-                     for($intNombreDeProduits=1;$intNombreDeProduits <= $_SESSION['NombreDeProduits'];$intNombreDeProduits++)
-                     {
-                        // Je fais les insertions à l'aide d'une requête préparée.
-                        $objPreparation = $objBDD->prepare('INSERT INTO Contenir(RefProduit, NumCommande, QteCommande, PrixDuJour) VALUES(:RefProduit, :NumCommande, :QteCommande, :PrixDuJour)');
-                        $objPreparation->execute(array(
-                           'RefProduit' => F_RefDuProduit($_SESSION['Panier']['DesignProduit'][$intNombreDeProduits]),
-                           'NumCommande' => $intDerniereID,
-                           'QteCommande' => $_SESSION['Panier']['QuantiteProduit'][$intNombreDeProduits],
-                           'PrixDuJour' => $_SESSION['Panier']['PrixDuJour'][$intNombreDeProduits]));
-                     }
-                     echo '<p class="centrer">';
-                     echo 'Commande ajoutée !';
-                     echo '<br />';
-                     echo '<a href=../index.php>Retour à l\'index</a>';
-                     echo '</p>';
-                     // Détruit la variable SESSION puisque l'on à fini !
-                     session_destroy();
+                     // Pour que ça soit plus clair, j'affecte dabord dans une variable avant de placer en parametre.
+                     // stripslashes au cas ou un produit est un appostrophe.
+                     $intRefProduit = F_RefDuProduit(stripslashes($_SESSION['Panier']['DesignProduit'][$intNombreDeProduits]));
+                     $fltQteCommande = $_SESSION['Panier']['QuantiteProduit'][$intNombreDeProduits];
+                     $fltPrixDuJour = $_SESSION['Panier']['PrixDuJour'][$intNombreDeProduits];
+                     P_AjoutDesProduits($intRefProduit,$intDerniereID,$fltQteCommande,$fltPrixDuJour); // Appel de la fonction P_AjoutDesProduit.
                   }
-                  catch(Exception $e)
-                  {
-                     die('Erreur : '.$e->getMessage());
-                  }
+                  echo '<p class="centrer">';
+                  echo 'Commande ajoutée !';
+                  echo '<br />';
+                  echo '<a href=../index.php>Retour à l\'index</a>';
+                  echo '</p>';
+                  // Détruit la variable SESSION puisque l'on à fini !
+                  session_destroy();
                }
                else
                {
