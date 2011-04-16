89e22560285f63c8b20077f7ae1f906b07b53dcd
fl4t <julien.stechele@gmail.com>
2011-04-16 21:06:18 +0200

    presentation de l'onglet consultation

--- consultation.php
+++ consultation.php
@@ -5,6 +5,19 @@
                <li><a href="administration.php">Administration</a></li>
             </ul>
          </nav>
+         <div id="panneau">
+            <ul>
+               <li><a href="traitement/traitement-ancien.php">Ancienne commande</a></li>
+               <li><a href=""></a></li>
+               <li><a href=""></a></li>
+            </ul>
+         </div>
          <div id="texte">
+            <blockquote>
+               <h2>Ancienne commande</h2>
+               <p>
+                  <strong>Ancienne commande</strong> permet d'analyser les commandes précédemment passé. 
+               </p>
+            </blockquote>
          </div>
          <?php include("footer.php");?>
